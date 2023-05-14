cb9e4dc740d76d061a5520c20a712ac9b85ee0da
Saif-eddeen <sdsdseao99@hotmail.com>
2023-05-14 14:52:22 +0300

    first commit

---
+++ dist/index.4f492e9b.js
@@ -0,0 +1,22 @@
+// // var mIsOpen = false;
+// var menu = document.getElementById("#mainMenu");
+// function openCloseMenu(){
+//     // if(!mIsOpen){
+//     //     menu.classList.remove("hidden");
+//     //     menu.classList.add("flex");
+//     //     mIsOpen = true;
+//     // }else {
+//     //     menu.classList.remove("flex");
+//     //     menu.classList.add("hidden");
+//     //     mIsOpen = false;
+//     // }
+//     menu.classList.toggle('hidden','flex');
+// }
+(function() {
+    const menuToggle = document.querySelector(".menu-toggle");
+    menuToggle.addEventListener("click", ()=>{
+        document.body.classList.toggle("menu-active");
+    });
+})();
+
+//# sourceMappingURL=index.4f492e9b.js.map
